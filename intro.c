27349d436f2cb5e9b0d8a867e04e68e68723079a
Vinay Jangam <vinayoffsec@gmail.com>
2026-09-15 19:20:35 +0530

    intro file for c language series !
    The first file added in this repo !  :3

---
+++ intro.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // header file , this header files lets us use functions like printf() and many more.
+
+int main() // it's a special function from where the program starts running any code inside the {} will be executed!
+{
+    char name[] = "vinay"; // variable of data type char named "name" holding value of string "vinay".
+
+
+
+    printf("%s\n", name); // using printf() function to print the variable name's value.
+
+    return 0; // ends the main function and sends a value '0' back to the operating system .
+    // returning a '0' usually means "everything worked"
+
+}
