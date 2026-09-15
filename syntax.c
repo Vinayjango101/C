9991bfcb210a10ec1d1c6eaeed83d4ae0c33fa4b
Vinay Jangam <vinayoffsec@gmail.com>
2026-09-15 19:22:56 +0530

    C Lang syntax file uploaded
    This file demonstrates the syntax used in C language .

---
+++ syntax.c
@@ -0,0 +1,21 @@
+//
+// Created by User on 15-09-2026.
+//
+
+// Every C statement ends with a semicolon ;
+/*#include <stdio.h>
+
+int main()
+{
+    printf("Hello World! From Syntax File ! :3");
+    return 0;
+}*/
+
+//------------------------------------ALTERNATE WAY TO WRITE CODE--------------------------------------------
+
+#include <stdio.h>
+
+int main(){printf("The same thing can also be written in this vertical way !");return 0;}
+
+
+
